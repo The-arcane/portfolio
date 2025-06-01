86c1545ad56a4e9c248477aaea1c51bf51c14642
Raunaq Adlakha <raunaq.adlakha@gmail.com>
2025-06-01 11:19:47 +0000

    still projects in last no issue u rollback i will go with previous version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ export default function Home() {
   return (
     <>
       <AboutSection />
-      <ResumeSection />
       <ProjectsSection />
+      <ResumeSection />
       <ContactSection />
     </>
   );
 }
+
