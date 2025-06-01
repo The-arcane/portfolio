32d07cbe259c5c39b417c3b3b4ce07a23cc22d99
Raunaq Adlakha <raunaq.adlakha@gmail.com>
2025-06-01 10:51:42 +0000

    i have added both profile-pic.jpeg and resume.pdf in root folder

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -1,3 +1,4 @@
+
 import { userProfileData } from '@/data/mock';
 import { SectionWrapper } from '@/components/shared/section-wrapper';
 import Image from 'next/image';
@@ -10,7 +11,7 @@ export function AboutSection() {
       <div className="grid md:grid-cols-3 gap-8 md:gap-12 items-center">
         <div className="md:col-span-1 flex justify-center md:justify-start">
            <Avatar className="w-48 h-48 md:w-64 md:h-64 border-4 border-accent shadow-lg">
-            <AvatarImage src="/profile-pic.jpg" alt={userProfileData.name} data-ai-hint="profile picture" />
+            <AvatarImage src="/profile-pic.jpeg" alt={userProfileData.name} data-ai-hint="profile picture" />
             <AvatarFallback className="text-4xl bg-primary text-primary-foreground">{userProfileData.name.substring(0,2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </div>
