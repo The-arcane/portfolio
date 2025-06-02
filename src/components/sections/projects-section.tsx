4d723b1666fe5b0e864d8dacfccf104796a16433
Raunaq Adlakha <raunaq.adlakha@gmail.com>
2025-06-02 14:17:38 +0000

    first of all header should be in top left and also my projects are showing like this expand the boaxes to full width

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -5,7 +5,7 @@ import { ProjectCard } from '@/components/shared/project-card';
 export function ProjectsSection() {
   return (
     <SectionWrapper id="projects" title="My Projects">
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-8"> {/* Changed lg:grid-cols-3 to lg:grid-cols-2 */}
         {projectsData.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
